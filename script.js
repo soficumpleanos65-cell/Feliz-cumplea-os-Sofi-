1d58b96831bb0f992fe67de28e5412997d504b74
soficumpleanos65-cell <soficumpleanos65@gmail.com>
2025-10-02 06:29:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// Configuración básica de confeti
+const canvas = document.getElementById('confetiCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let confetis = [];
+
+function Confeti() {
+  this.x = Math.random() * canvas.width;
+  this.y = Math.random() * canvas.height - canvas.height;
+  this.size = Math.random() * 8 + 4;
+  this.speed = Math.random() * 3 + 2;
+  this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+}
+
+function dibujarConfeti() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  confetis.forEach((c, i) => {
+    ctx.beginPath();
+    ctx.arc(c.x, c.y, c.size, 0, Math.PI * 2);
+    ctx.fillStyle = c.color;
+    ctx.fill();
+    c.y += c.speed;
+    if (c.y > canvas.height) {
+      confetis.splice(i, 1);
+    }
+  });
+  requestAnimationFrame(dibujarConfeti);
+}
+
+document.getElementById("confetiBtn").addEventListener("click", () => {
+  for (let i = 0; i < 200; i++) {
+    confetis.push(new Confeti());
+  }
+});
+
+// Ajustar tamaño al cambiar ventana
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
